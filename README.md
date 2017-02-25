d13db128258278846be99fb207f27eddebfc1875
Duda Nogueira <dudanogueira@gmail.com>
2017-02-25 10:30:14 -0300

    - Adiciona XSDs - Refatora para distribuição pip - Outros

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # TISS PARSER
 Análise e Validação do Padrão TISS da ANS
 
+Instalação (breve):
+
+pip install tiss
+
 Uso:
 ```
->> from tiss import Tiss
->> t = Tiss("arquivo-valido.xml")
+>> from tiss import Parser
+>> t = Parser("arquivo-valido.xml")
 >> t.valido
 True
 
->> t = Tiss("arquivo_invalido.xml")
+>> t = Parser("arquivo_invalido.xml")
 >> t.valido
 False
 >> t.tiss_erro
-"Erro apresentado"
+["Erro apresentado"]
 ```
 
 ## Validações Realizadas
 - Identifica a versao do TISS
 - Valida a estrutura com base no Schema XSD da ANS
 - Calcula e valida o hash
+
+## ROADMAP
+- Permitir plugins para validacao de carteirinha, e confronto com base de dados
+- Melhorar sistema de logs, permitindo modo verboso
+- Outros
--- tiss/__init__.py
+++ tiss/__init__.py
@@ -2,62 +2,82 @@
 #! /usr/bin/python
 from lxml import etree
 import os
-
-
 import hashlib
 
 
-class Tiss(object):
+__author__ = 'Duda Nogueira'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
+class Parser(object):
 
     def __init__(self, arquivo, xsd_path=None):
         self.arquivo = arquivo
         self.version = None
         self.xsd_path = xsd_path
-        self.xsd_valid = False
-        self.xsd_error = None
-        self.tiss_valid = False
-        self.tiss_error = None
+        self.xsd_valido = False
+        self.xsd_erros = []
+        self.valido = False
+        self.erros = []
+        self.arquivo_xsd = None
+        # analisa o arquivo
         self.parse()
-        self.get_version()
-        if self.version:
-            self.xsd_validate()
-            self.tiss_validate()
+        if self.arquivo_xsd:
+            # descobre versao
+            self.get_version()
+            if self.version:
+                # valida o xsd
+                self.xsd_validate()
+                if self.xsd_valido:
+                    # valida o arquivo
+                    self.tiss_validate()
+        xml_errors = None
 
     def parse(self):
         self.root = etree.fromstring(open(self.arquivo).read())
+        self.tipo_transacao = self.root.xpath(
+            '//ans:tipoTransacao', namespaces=self.root.nsmap)[0].text
+        if self.tipo_transacao == 'ENVIO_LOTE_GUIAS':
+            self.arquivo_xsd = 'tiss'
+
+        if not self.arquivo_xsd:
+            self.valido = False
+            self.erros.append('Tipo de Transação não identificado ou não suportado: %s')
+       
+
 
     def get_version(self):
         try:
             self.version = self.root.xpath(
                 '//ans:versaoPadrao', namespaces=self.root.nsmap)[0].text
         except:
-            self.tiss_valid = False
-            self.tiss_error = "Couldn't determine the tiss version"
+            self.valid = False
+            self.errors.append("Não foi possível determinar a versão TISS")
 
     def xsd_validate(self):
+
         if not self.xsd_path:
-            xsd_file = 'xsd/%s/tissV%s.xsd' % (
-                self.version, self.version.replace('.', '_'))
-            self.xsd_path = os.path.join(os.getcwd(), xsd_file)
+            xsd_file = 'xsd/%sV%s.xsd' % (self.arquivo_xsd, self.version.replace('.', '_'))
+            self.xsd_path = os.path.join(os.path.dirname(__file__), xsd_file)
         f = open(self.xsd_path)
         self.root_xsd = etree.parse(f)
         try:
             self.xsd_schema = etree.XMLSchema(self.root_xsd)
-            self.xsd_valid = True
-        except etree.XMLSchemaParseError as e:
-            self._valid = False
-            self.xsd_error = e
-            exit(1)
+            self.xsd_valido = True
+        except etree.XMLSchemaParseError, xsd_erros:
+            self.xsd_valido = False
+            self.xsd_erros = xsd_erros.error_log
+            self.erros.append(u'XSD Inválido!')
 
     def tiss_validate(self):
         try:
             self.xsd_schema.assertValid(self.root)
-            self.tiss_valid = True
+            self.valido = True
             self.calcula_hash()
             self.valida_hash()
-        except etree.DocumentInvalid as e:
-            self.tiss_valid = False
-            self.tiss_error = e
+        except etree.DocumentInvalid, xml_errors:
+            self.valid = False
+            self.erros = xml_errors.error_log
 
     def calcula_hash(self):
         # remove epilogo do calculo
@@ -71,13 +91,14 @@ class Tiss(object):
         for i in reslist:
             if not i.getchildren():
                 conteudos.append(i.text)
-        self.conteudo = ''.join(conteudos) 
+        self.conteudo = ''.join(conteudos)
         cod = hashlib.md5()
         cod.update(self.conteudo.encode('iso-8859-1'))
         self.hash = cod.hexdigest()
-    
+        self.parse()
+
     def valida_hash(self):
         if self.hash != self.hash_fornecido:
-            self.tiss_valid = False
-            self.tiss_error = "Hash Inválido! Fornecido: %s, Calculado: %s" % (self.hash_fornecido, self.hash)
-
+            self.valido = False
+            self.erros.append("Hash Inválido! Fornecido: %s, Calculado: %s" % (
+                self.hash_fornecido, self.hash))
