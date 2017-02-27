2010a9d353a7344a05b190d45e9fdeae3be8bdd7
Duda Nogueira <dudanogueira@gmail.com>
2017-02-27 00:50:44 -0300

    Adiciona checagem de senhas > beneficiario

--- tiss/extensoes/plugins/senha/senha_plugin.py
+++ tiss/extensoes/plugins/senha/senha_plugin.py
@@ -25,7 +25,23 @@ class PluginModelo(IPlugin):
             if senha not in senhas_unicas:
                 senhas_unicas.append(senha)
                 # confere  com senhas disponiveis no provider
+                try:
+                    carteira_senha = objeto.providers['senha'][int(senha)]['carteira']
+                    if not str(carteira_senha) == carteira:
+                    erro = {
+                        'numero': numero,
+                        'tag': "//ans:senha",
+                        'mensagem': u"Senha %s Encontrada no Autorizador, porém o Código do Beneficiário apontado é diferente do Código Autorizado." % senha
+                    }
+                    objeto.registra_erro_guia(erro)        
 
+                except KeyError:
+                    erro = {
+                        'numero': numero,
+                        'tag': "//ans:senha",
+                        'mensagem': u"Senha %s Não encontrada no Sistema Autorizado" % senha
+                    }
+                    objeto.registra_erro_guia(erro)    
             # senha ja usada nessa guia
             else:
                 erro = {
